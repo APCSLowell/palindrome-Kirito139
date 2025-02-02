e015cc493452e130a9fd7650bcb11f196d3d4731
Lennier Mulder <99382674+Kirito139@users.noreply.github.com>
2025-02-01 22:52:59 -0800

    Tests failed, reverting to default.

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -21,11 +21,9 @@ public class PalindromeChecker {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-        String foobar = new String();
         System.out.println("there are " + lines.length + " lines");
         for (int i = 0; i < lines.length; i++) {
-            foobar = onlyLetters(lines[i]).toLowerCase();
-            if (palindrome(foobar)) {
+            if (palindrome(lines[i]) == true) {
                 System.out.println(lines[i] + " IS a palindrome.");
             } else {
                 System.out.println(lines[i] + " is NOT a palindrome.");
@@ -33,20 +31,12 @@ public class PalindromeChecker {
         }
     }
     public boolean palindrome(String word) {
-        return reverse(word).equals(word);
+        // your code here
+        return false;
     }
-
     public String reverse(String str) {
-        String foo = new String();
-        for (int i = str.length()-1; i >= 0; i--) { foo += str.charAt(i); }
-        return foo;
-    }
-
-    public String onlyLetters(String str) {
-        String bar = "";
-        for (int i = 0; i < str.length(); i++) {
-            if (Character.isLetter(str.charAt(i))) bar += str.charAt(i);
-        }
-        return bar;
+        String sNew = new String();
+        // your code here
+        return sNew;
     }
 }

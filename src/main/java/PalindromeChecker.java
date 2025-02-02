da676654db5df09102f2c4480b19f30261889d68
Lennier Mulder <99382674+Kirito139@users.noreply.github.com>
2025-02-01 23:24:45 -0800

    Re-adding code.

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -21,9 +21,11 @@ public class PalindromeChecker {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        String foobar = new String();
         System.out.println("there are " + lines.length + " lines");
         for (int i = 0; i < lines.length; i++) {
-            if (palindrome(lines[i]) == true) {
+            foobar = onlyLetters(lines[i]).toLowerCase();
+            if (palindrome(foobar)) {
                 System.out.println(lines[i] + " IS a palindrome.");
             } else {
                 System.out.println(lines[i] + " is NOT a palindrome.");
@@ -31,12 +33,20 @@ public class PalindromeChecker {
         }
     }
     public boolean palindrome(String word) {
-        // your code here
-        return false;
+        return reverse(word).equals(word);
     }
+
     public String reverse(String str) {
-        String sNew = new String();
-        // your code here
-        return sNew;
+        String foo = new String();
+        for (int i = str.length()-1; i >= 0; i--) { foo += str.charAt(i); }
+        return foo;
+    }
+
+    public String onlyLetters(String str) {
+        String bar = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isLetter(str.charAt(i))) bar += str.charAt(i);
+        }
+        return bar;
     }
 }
